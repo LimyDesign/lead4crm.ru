d57b56238da496956db36969d48b3f52c21897a3
Arsen Bespalov <me@arsen.pw>
2015-03-28 16:15:12 +0800

    update VK auth & add OK auth

--- index.js
+++ index.js
@@ -46,18 +46,30 @@ app.get('/', function(req, res) {
 	else
 		cabinet = false;
 
+	var oauth_state = crypto.createHmac('sha1', req.headers['user-agent'] + new Date().getTime()).digest('hex');
+	req.session.oauth_state = oauth_state;
+
 	var vklogin_query = querystring.stringify({
 		client_id: process.env.VK_CLIENT_ID,
-		scope: 'notify,email',
+		scope: 'email',
 		redirect_uri: 'http://' + req.headers.host + '/vklogin',
 		response_type: 'code',
 		v: '5.29',
-		state: crypto.createHmac('sha1', req.headers['user-agent'] + new Date().getTime()).digest('hex'),
+		state: oauth_state,
 		display: 'page'
 	});
+	var oklogin_query = querystring.stringify({
+		client_id: process.env.OK_CLIENT_ID,
+		scope: 'GET_EMAIL',
+		response_type: 'code',
+		redirect_uri: 'http://' + req.headers.host + '/oklogin',
+		layout: 'w',
+		state: oauth_state
+	})
 	res.render('index.jade', {
 		title: 'Генератор лидов для Битрикс24',
 		vklogin: 'https://oauth.vk.com/authorize?' + vklogin_query,
+		oklogin: 'http://www.odnoklassniki.ru/oauth/authorize?' + oklogin_query,
 		mainpage_url: 'http://' + req.headers.host,
 		cabinet: cabinet,
 		cabinet_url: 'http://' + req.headers.host + '/cabinet'
@@ -88,25 +100,25 @@ app.get('/vklogin', function(req, res) {
 			if (arg == 'httpsreq') {
 				var httpsreq = https.request(options, function(res) {
 					res.setEncoding('utf8');
-					res.on('data', function(chunk) {
-						console.log('BODY: ' + chunk);
-						vk_res = JSON.parse(chunk);
-					});
-					if (res.statusCode != 200)
+					if (res.statusCode != 200) {
 						callback('error');
-					else
+					} else {
+						res.on('data', function(chunk) {
+							vk_res = JSON.parse(chunk);
+						});
 						callback(arg);
+					}
 				});
 				httpsreq.end();
 			} else if (arg == 'pgconnect') {
-				pg.connect(dbconfig, function(err, client, done) {
+				var client = new pg.Client(dbconfig);
+				client.connect(function(err) {
 					if (err) {
 						return console.error('Ошибка подключения к БД',err);
 					}
 					client.query('select * from users where vk = $1', [vk_res.user_id], function(err, result) {
-						done();
 						if (err) {
-							console.error('Ошибка получения данных',err);
+							return console.error('Ошибка получения данных',err);
 						} else {
 							if (result.rows[0]) {
 								console.log(result.rows[0]);
@@ -114,26 +126,25 @@ app.get('/vklogin', function(req, res) {
 								req.session.userid = result.rows[0].id;
 								req.session.user_email = result.rows[0].email;
 								req.session.vk = result.rows[0].vk;
-								req.session.vk_token = result.rows[0].vk_token;
+								client.end();
+								callback(arg);
 							} else {
-								console.log('Попытка создания нового пользователя. ');
+								console.log('Попытка создания нового пользователя.');
 								client.query("insert into users (email, vk) values ('" + vk_res.email + "', " + vk_res.user_id + ") returning id", function(err, result) {
-									done();
 									if (err) {
-										console.error('Ошибка записи данных в БД', err);
+										return console.error('Ошибка записи данных в БД', err);
 									} else {
 										req.session.authorized = true;
 										req.session.userid = result.rows[0].id;
 										req.session.user_email = result.rows[0].email;
 										req.session.vk = result.rows[0].vk;
-										req.session.vk_token = result.rows[0].vk_token;
 										console.log('Добавлен новый пользователь # ' + result.rows[0].id);
 									}
+									client.end();
+									callback(arg);
 								});
 							}
-							client.end();
 						}
-						callback(arg);
 					});
 				});
 			}
@@ -171,17 +182,154 @@ app.get('/vklogin', function(req, res) {
 });
 
 app.get('/oklogin', function(req, res) {
-	// res.send(req.headers);
-	// var shasum = crypto.createHmac('sha1', req.headers['user-agent'] + new Date().getTime());
-	// var d = shasum.digest('hex');
-	// res.send(crypto.createHmac('sha1', req.headers['user-agent'] + new Date().getTime()).digest('hex'));
+	console.log('Авторизация через соц.сеть "Одноклассники"'.green);
+
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var data = querystring.stringify({
+		client_id: process.env.OK_CLIENT_ID,
+		client_secret: process.env.OK_SECRET_KEY,
+		code: query.code,
+		redirect_uri: 'http://' + req.headers.host + '/oklogin',
+		grant_type: 'authorization_code'
+	});
+	var options = {
+		host: 'api.odnoklassniki.ru',
+		port: 443,
+		path: '/oauth/token.do',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': data.length
+		}
+	};
+
+	function async(arg, callback) {
+		setTimeout(function() {
+			console.log('Выполенение комманды ' + arg + '...');
+			if (arg == 'httpsreq') {
+				var httpsreq = https.request(options, function(res) {
+					res.setEncoding('utf8');
+					if (res.statusCode != 200) {
+						callback('error');
+					} else {
+						res.on('data', function(chunk) {
+							ok_res = JSON.parse(chunk);
+						});
+						callback(arg);
+					}
+				});
+				httpsreq.write(data);
+				httpsreq.end();
+			} else if (arg == 'get_user_data') {
+				var con_parameters = 'application_key=' + process.env.OK_PUBLIC_KEY + 'fields=uid,emailmethod=users.getCurrentUser';
+				var ac_ask = ok_res.access_token + process.env.OK_SECRET_KEY;
+				var md5_ac_ask = crypto.createHash('md5').update(ac_ask).digest('hex');
+				var sig = con_parameters + md5_ac_ask;
+				var md5_sig = crypto.createHash('md5').update(sig).digest('hex');
+				data = querystring.stringify({
+					application_key: process.env.OK_PUBLIC_KEY,
+					method: 'users.getCurrentUser',
+					access_token: ok_res.access_token,
+					fields: 'uid,email',
+					sig: md5_sig
+				});
+				options = {
+					host: 'api.ok.ru',
+					port: 80,
+					path: '/fb.do?' + data,
+					method: 'GET'
+				};
+				var httpreq = http.request(options, function(res) {
+					res.setEncoding('utf8');
+					res.on('data', function(chunk) {
+						ok_res2 = JSON.parse(chunk);
+					});
+					if (res.statusCode != 200)
+						callback('error');
+					else
+						callback(arg);
+				});
+				httpreq.end();
+			} else if (arg == 'pgconnect') {
+				var client = new pg.Client(dbconfig);
+				client.connect(function(err) {
+					if (err) {
+						return console.error('Ошибка подключения к БД',err);
+					}
+					client.query('select * from users where ok = $1', [ok_res2.uid], function(err, result) {
+						if (err) {
+							return console.error('Ошибка получения данных',err);
+						} else {
+							if (result.rows[0]) {
+								console.log(result.rows[0]);
+								req.session.authorized = true;
+								req.session.userid = result.rows[0].id;
+								req.session.user_email = result.rows[0].email;
+								req.session.ok = result.rows[0].ok;
+								client.end();
+								callback(arg);
+							} else {
+								console.log('Попытка создания нового пользователя. ');
+								ok_res2.email = ok_res2.email || 'you@email.com';
+								client.query("insert into users (email, ok) values ('" + ok_res2.email + "', " + ok_res2.uid + ") returning id", function(err, result) {
+									if (err) {
+										return console.error('Ошибка записи данных в БД', err);
+									} else {
+										req.session.authorized = true;
+										req.session.userid = result.rows[0].id;
+										req.session.user_email = result.rows[0].email;
+										req.session.ok = result.rows[0].ok;
+										console.log('Добавлен новый пользователь # ' + result.rows[0].id);
+									}
+									client.end();
+									callback(arg);
+								});
+							}
+						}
+					});
+				});
+			}
+		}, 4);
+	}
+
+	function final() {
+		console.log('Готовчик!'.yellow, results);
+		if (req.session.authorized && results.indexOf('error') < 0) {
+			res.redirect('http://' + req.headers.host + '/cabinet');
+		} else {
+			res.redirect('http://' + req.headers.host);
+		}
+	}
+
+	var ok_res;
+	var ok_res2;
+	var items = ["httpsreq","get_user_data","pgconnect"];
+	var results = [];
+
+	function series(item) {
+		if (item) {
+			async(item, function(result) {
+				results.push(result);
+				if (result == 'error')
+					return final();
+				else 
+					return series(items.shift());
+			});
+		} else {
+			return final();
+		}
+	}
+
+	series(items.shift());
 });
 
-app.get('/logout', function(req, res) {
+app.get('/logout', function(req, res, next) {
 	console.log('Выход из личного кабинета'.red);
 	req.session.destroy(function(err) {
 		if (err) {
 			console.log('Ошибка удаления сессии', err);
+			next();
 		} else {
 			res.redirect('http://' + req.headers.host);
 		}
@@ -197,7 +345,8 @@ app.get('/cabinet', function(req, res) {
 			mainpage_url: 'http://' + req.headers.host,
 			cabinet_url: 'http://' + req.headers.host + '/cabinet',
 			user_email: req.session.user_email,
-			vk_id: req.session.vk
+			vk_id: req.session.vk,
+			ok_id: req.session.ok
 		});
 	}
 
