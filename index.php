f5b8cde9b29daa7e80c1de9fc1e0215b4430f471
Arsen Bespalov <me@arsen.pw>
2015-05-10 15:57:28 +0800

    initial new site

--- index.php
+++ index.php
@@ -3,6 +3,16 @@ session_start();
 require_once __DIR__.'/src/vendor/autoload.php';
 $conf = json_decode(file_get_contents(__DIR__.'/config.json'), true);
 
+use Everzet\Jade\Dumper\PHPDumper,
+	Everzet\Jade\Visitor\AutotagsVisitor,
+	Everzet\Jade\Filter\JavaScriptFilter,
+	Everzet\Jade\Filter\CDATAFilter,
+	Everzet\Jade\Filter\PHPFilter,
+	Everzet\Jade\Filter\CSSFilter,
+	Everzet\Jade\Parser,
+	Everzet\Jade\Lexer\Lexer,
+	Everzet\Jade\Jade;
+
 $dumper = new PHPDumper();
 $dumper->registerVisitor('tag', new AutotagsVisitor());
 $dumper->registerFilter('javascript', new JavaScriptFilter());
