2bc7ea1328c0e7b094c0edef2f93384ec3cfbe3c
Arsen Bespalov <me@arsen.pw>
2015-05-10 16:15:15 +0800

    update site

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ session_start();
 require_once __DIR__.'/src/vendor/autoload.php';
 $conf = json_decode(file_get_contents(__DIR__.'/config.json'), true);
 
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false, true);
+
 use Everzet\Jade\Dumper\PHPDumper,
 	Everzet\Jade\Visitor\AutotagsVisitor,
 	Everzet\Jade\Filter\JavaScriptFilter,
@@ -15,13 +17,23 @@ use Everzet\Jade\Dumper\PHPDumper,
 
 $dumper = new PHPDumper();
 $dumper->registerVisitor('tag', new AutotagsVisitor());
-$dumper->registerFilter('javascript', new JavaScriptFilter());
-$dumper->registerFilter('cdata', new CDATAFilter());
 $dumper->registerFilter('php', new PHPFilter());
+$dumper->registerFilter('cdata', new CDATAFilter());
 $dumper->registerFilter('style', new CSSFilter());
+$dumper->registerFilter('javascript', new JavaScriptFilter());
 
 $parser = new Parser(new Lexer());
 $jade = new Jade($parser, $dumper);
+
+$requestURI = explode('/',$_SERVER['REQUEST_URI']);
+$scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
+for ($i=0;$i<sizeof($scriptName);$i++)
+{
+	if ($requestURI[$i] == $scriptName[$i])
+		unset($requestURI[$i]);
+}
+$cmd = array_values($requestURI);
+
 
 $template = __DIR__.'/views/index.jade';
 echo $jade->render($template);
