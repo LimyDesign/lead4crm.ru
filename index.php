10a1f97b9678dedbeda72b08b1da00cc40ee721b
Arsen Bespalov <me@arsen.pw>
2015-05-12 22:29:59 +0800

    update site

--- index.php
+++ index.php
@@ -31,6 +31,11 @@ if ($cmd[0]) {
 			getUserData();
 			break;
 
+		case 'getInvoice':
+			isAuth();
+			generateInvoice($_POST['invoicesum'], $_POST['companyname']);
+			break;
+
 		case 'cabinet':
 			isAuth();
 			$cOptions = array(
@@ -380,6 +385,149 @@ function getUserData() {
 	}
 	echo json_encode(array('balans' => $balans, 'tariff' => $tariff));
 	exit();
+}
+
+function generateInvoice($userSumm, $userCompany) {
+	global $pdf, $twig, $conf;
+
+	$pdf->SetCreator('CNAM RF');
+	$pdf->SetAuthor('Arsen Bespalov');
+	$pdf->SetTitle('CNAM RF Invoice');
+	$pdf->SetSubject('Invoice');
+	$pdf->SetKeywords('CNAM, invoice');
+
+	$pdf->setPrintHeader(false);
+	$pdf->setPrintFooter(false);
+
+	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+	$pdf->SetMargins(18, 10, 32, true);
+	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+	$pdf->SetFont('arial', '', 9);
+	$pdf->AddPage();
+
+	$user_sum = str_replace(',', '.', $userSumm);
+	$user_sum = str_replace(' ', '', $user_sum);
+	$sum = number_format($user_sum, 2, '-', ' ');
+	$sum_alt = number_format($user_sum, 2, '.', '\'');
+	$invoice_num = date('ymdHis').rand(0,9);
+	$html = $twig->render('invoice.twig', array(
+		'invoice_number' => 'L4CRM-'.writeInvoice($invoice_num, $user_sum),
+		'invoice_date' => russian_date().' г.',
+		'client_company' => setUserCompany($userCompany),
+		'userid' => $_SESSION['userid'],
+		'price' => $sum,
+		'summ' => $sum,
+		'summ_alt' => $sum_alt,
+		'total' => $sum,
+		'summ_text' => mb_ucfirst(num2str($user_sum))
+		));
+	$pdf->writeHTML($html, true, 0, true, 0);
+	$pdf->Image(K_PATH_IMAGES . 'print_trans.png', 21, 140, 40, '', '', '', '', false);
+	$pdf->Image(K_PATH_IMAGES . 'sign_trans.png', 50, 124, 60, '', '', '', '', false);
+	$pdf->lastPage();
+	$pdf->Output("Счет L4CRM-{$invoice_num}.pdf", 'D');
+}
+
+function writeInvoice($num, $sum, $system = 'bank') {
+	global $conf;
+	if ($conf['db']['type'] == 'postgres') {
+		$db = pg_connect('dbname='.$conf['db']['database']) or die('Невозможно подключиться к БД: '.pg_last_error());
+		$query = "insert into invoices (invoice, uid, sum, system) values ({$num}, {$_SESSION['userid']}, '{$sum}', '{$system}')";
+		pg_query($query);
+		pg_close($db);
+	}
+	return $num;
+}
+
+function setUserCompany($company) {
+	global $conf;
+	if ($company != $_SESSION['company']) {
+		if ($conf['db']['type'] == 'postgres') {
+			$db = pg_connect('dbname='.$conf['db']['database']) or die('Невозможно подключиться к БД: '.pg_last_error());
+			$company = pg_escape_string($company);
+			$query = "update users set company = '{$company}' where id = {$_SESSION['userid']}";
+			pg_query($query);
+			pg_close($db);
+			$_SESSION['company'] = $company;
+		}
+	}
+	return $company;
+}
+
+function russian_date() {
+	$date = explode('.',date('d.m.Y'));
+	switch ($date[1]) {
+		case 1: $m = 'января'; break;
+		case 2: $m = 'февраля'; break;
+		case 3: $m = 'марта'; break;
+		case 4: $m = 'апреля'; break;
+		case 5: $m = 'мая'; break;
+		case 6: $m = 'июня'; break;
+		case 7: $m = 'июля'; break;
+		case 8: $m = 'августа'; break;
+		case 9: $m = 'сентября'; break;
+		case 10: $m = 'октября'; break;
+		case 11: $m = 'ноября'; break;
+		case 12: $m = 'декабря'; break;
+	}
+	return $date[0].' '.$m.' '.$date[2];
+}
+
+function num2str($num) {
+    $nul='ноль';
+    $ten=array(
+        array('','один','два','три','четыре','пять','шесть','семь', 'восемь','девять'),
+        array('','одна','две','три','четыре','пять','шесть','семь', 'восемь','девять'),
+    );
+    $a20=array('десять','одиннадцать','двенадцать','тринадцать','четырнадцать' ,'пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать');
+    $tens=array(2=>'двадцать','тридцать','сорок','пятьдесят','шестьдесят','семьдесят' ,'восемьдесят','девяносто');
+    $hundred=array('','сто','двести','триста','четыреста','пятьсот','шестьсот', 'семьсот','восемьсот','девятьсот');
+    $unit=array( // Units
+        array('копейка' ,'копейки' ,'копеек',	 1),
+        array('рубль'   ,'рубля'   ,'рублей'    ,0),
+        array('тысяча'  ,'тысячи'  ,'тысяч'     ,1),
+        array('миллион' ,'миллиона','миллионов' ,0),
+        array('миллиард','милиарда','миллиардов',0),
+    );
+    //
+    list($rub,$kop) = explode('.',sprintf("%015.2f", floatval($num)));
+    $out = array();
+    if (intval($rub)>0) {
+        foreach(str_split($rub,3) as $uk=>$v) { // by 3 symbols
+            if (!intval($v)) continue;
+            $uk = sizeof($unit)-$uk-1; // unit key
+            $gender = $unit[$uk][3];
+            list($i1,$i2,$i3) = array_map('intval',str_split($v,1));
+            // mega-logic
+            $out[] = $hundred[$i1]; # 1xx-9xx
+            if ($i2>1) $out[]= $tens[$i2].' '.$ten[$gender][$i3]; # 20-99
+            else $out[]= $i2>0 ? $a20[$i3] : $ten[$gender][$i3]; # 10-19 | 1-9
+            // units without rub & kop
+            if ($uk>1) $out[]= morph($v,$unit[$uk][0],$unit[$uk][1],$unit[$uk][2]);
+        } //foreach
+    }
+    else $out[] = $nul;
+    $out[] = morph(intval($rub), $unit[1][0],$unit[1][1],$unit[1][2]); // rub
+    $out[] = $kop.' '.morph($kop,$unit[0][0],$unit[0][1],$unit[0][2]); // kop
+    return trim(preg_replace('/ {2,}/', ' ', join(' ',$out)));
+}
+
+function morph($n, $f1, $f2, $f5) {
+    $n = abs(intval($n)) % 100;
+    if ($n>10 && $n<20) return $f5;
+    $n = $n % 10;
+    if ($n>1 && $n<5) return $f2;
+    if ($n==1) return $f1;
+    return $f5;
+}
+
+function mb_ucfirst($str, $encoding='UTF-8') {
+   $str = mb_ereg_replace('^[\ ]+', '', $str);
+   $str = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding).
+          mb_substr($str, 1, mb_strlen($str), $encoding);
+   return $str;
 }
 
 function isAuth() {
