c8e114f60ae9378884784dccec94ea2c639737b3
Arsen Bespalov <me@arsen.pw>
2015-05-16 18:08:38 +0800

    update bx24 module

--- index.php
+++ index.php
@@ -62,9 +62,15 @@ if ($cmd[0]) {
 				'tariffs' => getUserTariff());
 
 		case 'b24-install':
+			$auth = $_REQUEST['AUTH_ID'];
+			$domain = ($_REQUEST['PROTOCOL'] == 0 ? 'http' : 'https') . '://'. $_REQUEST['DOMAIN'];
+			$isAdmin = json_decode(file_get_contents($domain.'/rest/user.admin.json?auth='.$auth));
+			$cOptions = array(
+				'isAdmin' => $isAdmin);
 		case 'b24-index':
 			$auth = $_REQUEST['AUTH_ID'];
 			$domain = ($_REQUEST['PROTOCOL'] == 0 ? 'http' : 'https') . '://'. $_REQUEST['DOMAIN'];
+			$isAdmin 
 			$res = file_get_contents($domain.'/rest/user.current.json?auth='.$auth);
 			$arRes = json_decode($res, true);
 			$cOptions = array(
