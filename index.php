6665addd3112d8e2046e3e5ca7e0d9616d6e43ca
Arsen Bespalov <me@arsen.pw>
2015-05-16 14:29:44 +0800

    update bx24 module

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if ($cmd[0]) {
 			$domain = ($_REQUEST['PROTOCOL'] == 0 ? 'http' : 'https') . '://'. $_REQUEST['DOMAIN'];
 			$res = file_get_contents($domain.'/rest/user.current.xml?auth='.$auth);
 			$arRes = json_encode($res, true);
-			var_dump($res); die();
+			var_dump($domain.'/rest/user.current.xml?auth='.$auth); die();
 			$cOptions = array(
 				'res' => $arRes,
 				'cities' => getCities());
