ea5c4c4aef03785d5574a97dfb333d733f63d2d6
Arsen Bespalov <me@arsen.pw>
2015-08-15 16:01:22 +0800

    update site: add webcall button

--- public/js/webcall.js
+++ public/js/webcall.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 	$('#webcall form').submit(function(event) {
 		event.preventDefault();
+		var action = $(this).attr('action');
+		var formData = $(this).serialize();
+		$.post(action, formData, function(data) {
+			$('#webcallmsg').removeClass('hide').html(data);
+		});
 	});
 });
