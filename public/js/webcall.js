a8f46db817a248eb2972b98b2e617b011d6bbc14
Arsen Bespalov <me@arsen.pw>
2015-08-16 21:08:26 +0800

    update site: add webcall button

--- public/js/webcall.js
+++ public/js/webcall.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	$('#webcall form').submit(function(event) {
 		event.preventDefault();
+		var _form = $(this);
 		var action = $(this).attr('action');
 		var formData = $(this).serialize();
+		$(this).find('fieldset').attr('disabled', 'disabled');
+		$('#webcallmsg').removeClass().addClass('alert alert-info').html('<i class="fa fa-fw spinner fa-pulse"></i> Попытка установить соединение...');
 		$.post(action, formData, function(data) {
 			if (data.Result)
 			{
@@ -12,6 +15,8 @@ $(document).ready(function() {
 			{
 				$('#webcallmsg').removeClass().addClass('alert alert-danger').text(data.ErrorStr);
 			}
-		}, 'json');
+		}, 'json').done(function() {
+			_form.find('fieldset').removeAttr('disabled');
+		});
 	});
 });
