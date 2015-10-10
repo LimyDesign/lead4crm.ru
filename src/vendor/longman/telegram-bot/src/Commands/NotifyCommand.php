932b92d0f1ee9d9ffbeb48bb9ad3eb4d70eb84ec
Arsen Bespalov <me@arsen.pw>
2015-10-10 12:35:39 +0800

    update telegram bot

--- src/vendor/longman/telegram-bot/src/Commands/NotifyCommand.php
+++ src/vendor/longman/telegram-bot/src/Commands/NotifyCommand.php
@@ -28,29 +28,15 @@ class NotifyCommand extends Command
         $message_id = $message->getMessageId();
         $text = $message->getText(true);
 
+        if (empty($text))
+        {
+                $reply = "Для того чтобы включить, отключить или проверить статус уведомления, для каждого действия введите следующие команды:\n\n/notify company [on|off|status] — Уведомление об импорте компании\n/notify renewal [on|off|status] — Уведомление о продлении тарифного плана\n/notify balans [on|off|status] - Уведомление об изменении баланса";
+        }
+
         $data = array();
         $data['chat_id'] = $chat_id;
-        $data['text'] = "Выберите раздел уведомлений:";
+        $data['text'] = $reply;
 
-        $keyboards = array();
-
-        // 0
-        $keyboard[] = ['Импорт компаний'];
-        $keyboard[] = ['Предупреждение о продлении тарифа'];
-        $keyboard[] = ['Уведомление о снижении баланса'];
-
-        $keyboards[] = $keyboard;
-        unset($keyboard);
-
-        $reply_keyboard_markup = new ReplyKeyboardMarkup(
-        	[
-        		'keyboard' => $keyboards[0],
-        		'resize_keyboard' => true,
-        		'one_time_keyboard' => true,
-        		'selective' => false
-        	]
-        );
-        $data['reply_markup'] = $reply_keyboard_markup;
 
         $result = Request::sendMessage($data);
         return $result;
--- src/vendor/longman/telegram-bot/src/Telegram.php
+++ src/vendor/longman/telegram-bot/src/Telegram.php
@@ -30,7 +30,7 @@ class Telegram
      *
      * @var string
      */
-    protected $version = '0.18.0';
+    protected $version = '0.1.0';
 
     /**
      * Telegram API key
