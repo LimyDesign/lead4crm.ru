ce9507de886cc57fed1574466ae8b03d2b4e3e21
Arsen Bespalov <me@arsen.pw>
2015-10-15 06:25:07 +0800

    update

--- icq_bot.php
+++ icq_bot.php
@@ -361,7 +361,7 @@ function shutdown() {
 	global $icq;
 	while ($icq->isConnected()) {
 		$icq->sendMessage(ADMINUIN, 'Service Lead4CRM Bot stoped...');
-		$icq->setStatus('STATUS_OFFLINE', 'STATUS_DCDISABLED', 'Goodbay!');
+		$icq->setStatus('STATUS_OFFLINE', 'STATUS_OFFLINE', 'Goodbay!');
 		sleep(1);
 		$icq->disconnect();
 	}
