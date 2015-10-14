af9d44b023578aeccc087870f76c3268b1d00dc6
Arsen Bespalov <me@arsen.pw>
2015-10-14 13:47:14 +0800

    update

--- icq_bot.php
+++ icq_bot.php
@@ -30,6 +30,8 @@ $about = "Lead4CRM Bot v1.0.0\n
 \tпредупредить о грядущем продлении вашего тарифного плана.\r
 ";
 
+$ignore_list = array();
+
 $message_response = array();
 
 while (1) {
@@ -79,7 +81,7 @@ while (1) {
 				!empty($msg['message']) && 
 				!in_array($msg['from'], $ignore_list)
 			) {
-				$icq->sendMessage(ADMINUIN, $msg['from'].'>'.trim($msg['message']));
+				// $icq->sendMessage(ADMINUIN, $msg['from'].'>'.trim($msg['message']));
 				switch (strtolower(trim($msg['message']))) {
 					case '!about':
 						$icq->sendMessage($msg['from'], mb_convert_encoding($about, 'cp1251'));
