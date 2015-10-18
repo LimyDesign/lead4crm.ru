af6a1a08d3b8047efe345f0a2001899eaabea3ed
Arsen Bespalov <me@arsen.pw>
2015-10-18 10:02:54 +0800

    update

--- index.php
+++ index.php
@@ -221,7 +221,7 @@ if ($cmd[0]) {
 				'admin' => $_SESSION['is_admin'],
 				'telegramid' => $_SESSION['telegramid'],
 				'icq' => $_SESSION['icq'],
-				'notify_danger' => (!isset($_SESSION['telegram'] && !isset($_SESSION['icq']))) ? true : false,
+				'notify_danger' => (!isset($_SESSION['telegram']) && !isset($_SESSION['icq'])) ? true : false,
 				'crm_list' => getCRM(),
 				'countries' => getCountries(getUserCityByIP()),
 				'top_rubrics' => $top_rubrics,
