20d7abed357e8ea986e14eb301a81321e8a5de0e
Arsen Bespalov <me@arsen.pw>
2015-10-28 21:22:52 +0800

    update

--- src/vendor/tecnick.com/tcpdf/config/tcpdf_config.php
+++ src/vendor/tecnick.com/tcpdf/config/tcpdf_config.php
@@ -45,7 +45,7 @@
  * Installation path (/var/www/tcpdf/).
  * By default it is automatically calculated but you can also set it as a fixed string to improve performances.
  */
-// define ('K_PATH_MAIN', __DIR__);
+define ('K_PATH_MAIN', __DIR__.'/');
 
 /**
  * URL path to tcpdf installation folder (http://localhost/tcpdf/).
