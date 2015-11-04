67a7e87c59866a3a1c86ae735e63a7ad5f8dff2f
Arsen Bespalov <me@arsen.pw>
2015-11-05 01:05:40 +0800

    update

--- public/js/crm/megaplan.js
+++ public/js/crm/megaplan.js
@@ -1,11 +1,9 @@
-$(function() {
-	$('#formMegaplanConnect').submit(function(event) {
-		event.preventDefault();
-		$('#megaplanConnect').modal('hide');
+$('#formMegaplanConnect').submit(function(event) {
+	event.preventDefault();
+	$('#megaplanConnect').modal('hide');
 
-	});
+});
 
-	$('#megaplanConnect #ok').click(function() {
-		$('#formMegaplanConnect').submit();
-	});
+$('#megaplanConnect #ok').click(function() {
+	$('#formMegaplanConnect').submit();
 });
