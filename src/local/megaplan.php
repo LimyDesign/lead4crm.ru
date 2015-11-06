65d5addc8925b012aa5f8c442850f86360f6ce9e
Arsen Bespalov <me@arsen.pw>
2015-11-06 15:14:26 +0800

    update

--- src/local/megaplan.php
+++ src/local/megaplan.php
@@ -49,8 +49,16 @@ class megaplan extends SdfApi_Request
 			"Model[TypePerson]" => "company",
 			"Model[CompanyName]" => "Хуй!!!",
 			"Model[Email]" => "mega@huy.org",
-			"Model[Phones]" => "ph_w-73952781089",
-			"Model[Responsibles]" => ""
+			"Model[Phones]" => "ph_w-7-3952-781089",
+			"Model[Responsibles]" => "1000000",
+			"Model[ActivityType]" => "Город / Власть",
+			"Model[Icq]" => "881129",
+			"Model[Skype]" => "arsen_loren",
+			"Model[Facebook]" => "https://www.fb.com/arsen.bespalov",
+			"Model[Twitter]" => "https://www.twitter.com/arsenbespalov",
+			"Model[Site]" => "https://www.arsen.pw",
+			"Model[AdvertisingWay]" => "2ГИС",
+			"Model[Type]" => "Клиент"
 		);
 		$result = $this->sdf->post('/BumsCrmApiV01/Contractor/save.api', $opt);
 		return $result;
