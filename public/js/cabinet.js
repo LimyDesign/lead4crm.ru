bc57202cc2ffa80900ce4fce0d37de5b4c6ba30f
Arsen Bespalov <me@arsen.pw>
2015-11-07 14:14:43 +0800

    update

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -80,7 +80,6 @@ $(document).ready(function()
         wzd_height = $('.wizard').height();
     $(this).find('fieldset').attr('disabled', 'disabled');
     $('.overlay').css({ 'top': wzd_pos.top, 'left': wzd_pos.left }).height(wzd_height).fadeIn('fast');
-    console.log(wzd_height);
 
     if ($('#ii').hasClass('hide') === false)
       $('#ii').addClass('hide');
@@ -153,9 +152,6 @@ $(document).ready(function()
     } else if (form_action == '/step-4/') {
       if (searchType != null) {
         if (ii != null) {
-          wzd_height = $('.wizard').height();
-          console.log(wzd_height);
-          $('.overlay').height(wzd_height);
           $('#ii').removeClass('hide');
           $('#ii_help').removeClass('hide');
           $.post('/getIntegrated/', { ii: ii }, function (data) {
