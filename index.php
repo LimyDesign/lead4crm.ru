42ac52e849a5de3e724e9d0c74b42fd0dffa0837
Arsen Bespalov <me@arsen.pw>
2015-11-08 21:10:49 +0800

    update

--- index.php
+++ index.php
@@ -489,6 +489,7 @@ function crmSaveSettings($crm) {
 	$crmid = pg_fetch_result($result, 0, 0);
 	if ($crmid) {
 		$crmClass = new $crm($crmid);
+		echo 'asdasd';
 		echo $crmClass->putSetting();
 	}
 }
