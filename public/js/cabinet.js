ac30a278a43ac8c4650ca54dc1cb7f81473e3134
Arsen Bespalov <me@arsen.pw>
2015-11-09 19:00:56 +0800

    update

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -768,6 +768,7 @@ function exportSelection(sDate, crm_id, post, increment, data) {
     } else {
       $.post('/crmPostCompany/'+ii+'/', { opt: data.opt[increment] }, function (res) {
         if (res) {
+          console.log(res);
           if (res.status.code == 'error') {
             alert(res.status.message);
             exportDialog.modal('hide');
