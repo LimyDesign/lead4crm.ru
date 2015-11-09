190da17f561cd70dd89703a8ed94c7822170132b
Arsen Bespalov <me@arsen.pw>
2015-11-09 14:05:15 +0800

    update

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -768,11 +768,11 @@ function exportSelection(sDate, crm_id) {
       percent = Math.round((i + 1) * 100 / data.total);
       if (percent >= 100) {
         clearInterval(q);
-        exportDialog.find('.progress-bar').attr('aria-valuenow', '0');
-        exportDialog.find('.progress-bar').css('width', '0');
-        exportDialog.find('.progress-bar').text('0%');
-        exportDialog.find('#companyName').text('n/a');
         setTimeout(function() {
+          exportDialog.find('.progress-bar').attr('aria-valuenow', '0');
+          exportDialog.find('.progress-bar').css('width', '0');
+          exportDialog.find('.progress-bar').text('0%');
+          exportDialog.find('#companyName').text('n/a');
           exportDialog.modal('hide');
         }, 2000);
       } else {
