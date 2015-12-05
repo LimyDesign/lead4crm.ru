6ef11656395ecc17fbd4256d2402ae84fdff105a
Arsen Bespalov <me@arsen.pw>
2015-12-05 21:12:48 +0800

    bug fix

--- index.php
+++ index.php
@@ -261,6 +261,7 @@ if ($cmd[0]) {
 
 		case 'cabinet':
 			isAuth($cmd);
+            echo "ffff";
 			$top_rubrics = importRubrics($_SESSION['apikey'], 'www.lead4crm.ru');
 			$top_rubrics = json_decode($top_rubrics, true);
 			$cOptions = array(
