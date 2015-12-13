668e7a9bd2d27b6dece560f06face27e66bec93b
Arsen Bespalov <me@arsen.pw>
2015-12-13 17:41:45 +0800

    bug fix

--- src/lib/Lead4CRM/API.php
+++ src/lib/Lead4CRM/API.php
@@ -1368,17 +1368,17 @@ class API
      * @return string Возвращает IP-адрес пользователя по возможности.
      */
     static public function getRealIpAddr() {
-        if ($_SERVER['HTTP_CLIENT_IP'])
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if ($_SERVER['HTTP_X_FORWARDED_FOR'])
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if ($_SERVER['HTTP_X_FORWARDED'])
+        else if (isset($_SERVER['HTTP_X_FORWARDED']))
             $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if ($_SERVER['HTTP_FORWARDED_FOR'])
+        else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
             $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if ($_SERVER['HTTP_FORWARDED'])
+        else if (isset($_SERVER['HTTP_FORWARDED']))
             $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if ($_SERVER['REMOTE_ADDR'])
+        else if (isset($_SERVER['REMOTE_ADDR']))
             $ipaddress = $_SERVER['REMOTE_ADDR'];
         else
             $ipaddress = '77.88.8.8';
