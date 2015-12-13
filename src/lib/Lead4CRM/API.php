001ec7112e3753315c3bd6d94ff176e88b1282d3
Arsen Bespalov <me@arsen.pw>
2015-12-13 17:59:03 +0800

    bug fix

--- src/lib/Lead4CRM/API.php
+++ src/lib/Lead4CRM/API.php
@@ -286,7 +286,7 @@ class API
             $cities = $this->getMultipleRows($sql, $param);
             $arrCities = array();
             foreach ($cities as $city) {
-                if (isset($city['parent_id'])) {
+                if ($city['parent_id']) {
                     $arrCities[$city['parent_id']]['children'] = $arrCities[$city['parent_id']]['children'] ? $arrCities[$city['parent_id']]['children'] . ', ' . $city['name'] : $city['name'];
                     if ($userCityName == $city['name'])
                         $arrCities[$city['parent_id']]['selected'] = 1;
