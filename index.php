f8ddbf5e2461631044bac01a60f9cc907a797b49
Arsen Bespalov <me@arsen.pw>
2015-12-14 01:37:57 +0800

    move gzip compress to php.ini

--- index.php
+++ index.php
@@ -18,11 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
 		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 }
 
-if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
-    ob_start('ob_gzhandler');
-else
-    ob_start();
-
 require_once __DIR__.'/src/vendor/autoload.php';
 
 $conf = json_decode(file_get_contents(__DIR__.'/config.json'));
