fa812d23fafa6bc9b2615fc9b30f9ecdd5b4ed73
Arsen Bespalov <me@arsen.pw>
2015-12-14 17:23:09 +0800

    update amoCRM widget

--- public/js/amocrm.js
+++ public/js/amocrm.js
@@ -3,7 +3,8 @@ var self = document.querySelector('script[data-name="amocrm"]'),
     usercity = getParamByName('uc');
 
 (function($){
-    var $citysearch = $('[id^=inputCitySearch]');
+    var $citysearch = $('[id^=inputCitySearch]'),
+        $inputRubricSearch = $('#inputRubricSearch');
     $.get('/getSupportCities/', function(data) {
         $citysearch.typeahead({
             source: data,
@@ -28,6 +29,58 @@ var self = document.querySelector('script[data-name="amocrm"]'),
     $citysearch.on('mouseup', function() {
         $(this).select();
     });
+
+    if (localStorage.getItem('2GISRubrics')) {
+        var rubrics = JSON.parse(localStorage.getItem('2GISRubrics'));
+        var level2 = null,
+            level3 = null;
+        rubrics.forEach(function (entry) {
+            if (!entry.parent) {
+                $inputRubricSearch
+                    .append('<option data-divider="true"></option>')
+                    .append('<option value="' + entry.id + '">'+entry.name+'</option>');
+                level2 = entry.id;
+            } else if (level2 == entry.parent) {
+                $inputRubricSearch
+                    .append('<option value="'+entry.id+'" data-content="<span class=\'text-muted\'>&mdash;&nbsp;</span><span class=\'text\'>'+entry.name+'</span>">'+entry.name+'</option>');
+                level3 = entry.id;
+            } else if (level3 == entry.parent) {
+                $inputRubricSearch
+                    .append('<option value="'+entry.id+'" data-content="<span class=\'text-muted\'>&mdash;&nbsp;&mdash;&nbsp;</span><span class=\'text\'>'+entry.name+'</span>">'+entry.name+'</option>');
+            }
+        });
+        $inputRubricSearch.selectpicker('render');
+    } else {
+        var postData = {
+            importAPI: getParamByName('apikey'),
+            importDomain: getParamByName('subdomain') + '.amocrm.ru'
+        };
+        $.post('/getRubricList/', postData, function (data) {
+            if (data.error == 0) {
+                if (data.rubrics) {
+                    localStorage.setItem('2GISRubrics', JSON.stringify(data.rubrics));
+                    var level2 = null,
+                        level3 = null;
+                    data.rubrics.forEach(function (entry) {
+                        if (!entry.parent) {
+                            $inputRubricSearch
+                                .append('<option data-divider="true"></option>')
+                                .append('<option value="'+entry.id+'">'+entry.name+'</option>');
+                            level2 = entry.id;
+                        } else if (level2 == entry.parent) {
+                            $inputRubricSearch
+                                .append('<option value="'+entry.id+'" data-content="<span class=\'text-muted\'>&mdash;&nbsp;</span><span class=\'text\'>'+entry.name+'</span>">'+entry.name+'</option>');
+                            level3 = entry.id;
+                        } else if (level3 == entry.parent) {
+                            $inputRubricSearch
+                                .append('<option value="'+entry.id+'" data-content="<span class=\'text-muted\'>&mdash;&nbsp;&mdash;&nbsp;</span><span class=\'text\'>'+entry.name+'</span>">'+entry.name+'</option>');
+                        }
+                    });
+                    $inputRubricSearch.selectpicker('render');
+                }
+            }
+        }, 'json');
+    }
 
     $('#formTextSearch').submit(function(e){
         e.preventDefault();
