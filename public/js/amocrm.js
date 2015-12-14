df23c5e8b88b598747e195e99608e1b4402f8c8c
Arsen Bespalov <me@arsen.pw>
2015-12-14 15:15:27 +0800

    update amoCRM widget

--- public/js/amocrm.js
+++ public/js/amocrm.js
@@ -1,8 +1,12 @@
+var self = document.querySelector('script[data-name="amocrm"]'),
+    script_src = self.getAttribute('src'),
+    usercity = getParamByName('uc');
+
 (function($){
     var $typeahead = $('#inputCitySearch');
     $.get('/getSupportCities/', function(data){
-
         $typeahead.typeahead({ source: data, autoselect: true });
+        $typeahead.val(usercity);
     }, 'json');
 
     $('#formTextSearch').submit(function(e){
@@ -10,3 +14,10 @@
         alert('Yo!');
     });
 })(jQuery);
+
+function getParamByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(script_src);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
