be7919da33438d5cde37c869d9464811fd8d4ff7
Arsen Bespalov <me@arsen.pw>
2015-12-15 12:04:59 +0800

    update amoCRM widget

--- public/js/amocrm.js
+++ public/js/amocrm.js
@@ -66,7 +66,7 @@ var self = document.querySelector('script[data-name="amocrm"]'),
         $('tbody tr').clone().appendTo('table');
     }
     $resultTable.stickyTableHeaders();
-    $body.css('height', screen.availHeight - $body.offset().top);
+    $body.css('height', screen.availHeight - $body.offset().top - 60);
 
     $tabs.on('shown.bs.tab', function(e) {
         if ($(e.target).attr('aria-controls') == 'result') {
