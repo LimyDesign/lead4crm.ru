3768b73c76c0e95349ae12e2b9cce61a97ff7b5a
Arsen Bespalov <me@arsen.pw>
2016-02-07 15:40:56 +0800

    update payment method: add debug file

--- src/lib/Lead4CRM/API.php
+++ src/lib/Lead4CRM/API.php
@@ -1346,7 +1346,7 @@ class API
             }
             $response .= "<paymentAvisoResponse performedDatetime=\"{$performedDatetime}\" code=\"{$code}\" invoiceId=\"{$yaInvoiceId}\" shopId=\"{$yaShopId}\"/>";
         }
-        file_put_contents('/var/www/html/ya_debug.log', $response);
+        file_put_contents('/var/www/html/ya_debug.log', $response, FILE_APPEND | LOCK_EX);
         return $response;
     }
 
