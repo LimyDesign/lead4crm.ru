d55cceecf0a151c54c95f7ad96242494afab2511
Arsen Bespalov <me@arsen.pw>
2016-02-27 16:52:40 +0800

    update referal program

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -1338,6 +1338,7 @@ function goto(page, tab) {
       data.forEach(function(entry, index) {
         var num = index + 1, _total = 0;
         _vk = _ok = _fb = _gp = _mr = _ya = uncheck;
+        if (entry.email == null) entry.email = '(адрес не указан)';
         if (entry.vk) _vk = check;
         if (entry.ok) _ok = check;
         if (entry.fb) _fb = check;
